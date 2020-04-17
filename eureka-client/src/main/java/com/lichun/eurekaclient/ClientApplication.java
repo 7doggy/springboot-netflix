30adf02f759b9f426ecbf6f340f615b26e21547e
7doggy <seven.doggy@gmail.com>
2020-04-17 12:20:13 +0800

    commit

--- eureka-client/src/main/java/com/lichun/eurekaclient/ClientApplication.java
+++ eureka-client/src/main/java/com/lichun/eurekaclient/ClientApplication.java
@@ -3,10 +3,10 @@ package com.lichun.eurekaclient;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class ClientApplication {
 
     public static void main(String[] args) {
